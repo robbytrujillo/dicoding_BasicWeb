73d35c55faaddfb4f2610a46566979367f868ee6
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-01-09 11:04:53 +0700

    Dicoding Web Basic - Dasar JS - Operator Penugasan

---
+++ belajar/WebCalculator/script/operatorPenugasan.js
@@ -0,0 +1,16 @@
+//Assignment Operator
+let x = 10;
+let y = 5
+
+x += y; // artinya -> x = x + y;
+x -= y; // artinya -> x = x - y;
+x *= y; // artinya -> x = x * y;
+x /= y; // artinya -> x = x / y;
+x %= y; // artinya -> x = x % y;
+
+console.log(x);
+
+/* output
+15
+*/
+
