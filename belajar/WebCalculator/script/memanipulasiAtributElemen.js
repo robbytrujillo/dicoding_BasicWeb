321037d49e35bfa76de66ecdcf4069cfe98b854e
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-01-12 06:57:48 +0700

    Dicoding Web Basic - Dasar JS - Memanipulasi Atribut elemen

--- belajar/WebCalculator/script/memanipulasiAtributElemen.js
+++ belajar/WebCalculator/script/memanipulasiAtributElemen.js
@@ -2,3 +2,5 @@ someElement.setAttribute("attributName", "attributeValue");
 
 let catImage = document.querySelector("#catImage");
 catImage.setAttribute("src", "https://i.ibb.co/55VG7vL/three-cat.jpg");
+
+catImage.getAttribute("src");
