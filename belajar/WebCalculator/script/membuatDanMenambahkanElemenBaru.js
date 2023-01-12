3da6cec873509eb31cf77896edaaf691995388c9
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-01-12 21:56:42 +0700

    Dicoding Web Basic - Dasar JS - Membuat dan Menambahkan Elemen Baru

---
+++ belajar/WebCalculator/script/membuatDanMenambahkanElemenBaru.js
@@ -0,0 +1,5 @@
+let newElement = document.createElement('p');
+
+newElement.innerHTML = 'Anda menekan gambar kucing sebanyak <span id="count">0</span> kali';
+
+document.body.appendChild(newElement);
