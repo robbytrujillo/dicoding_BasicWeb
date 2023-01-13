75ab771f764dc975ba6e8024902d2b9abbe59d45
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-01-13 22:34:59 +0700

    Dicoding Web Basic - Dasar JS - Menerpakan JS pada Web kalkulator

--- belajar/WebCalculator/script/kalkulator.js
+++ belajar/WebCalculator/script/kalkulator.js
@@ -12,4 +12,14 @@ function updateDisplay() {
     document.querySelector('#displayNumber').innerText = calculator.displayNumber;
 }
 
+function clearCalculator() {
+    calculator.displayNumber = '0';
+    calculator.operator = null;
+    calculator.firstNumber = null;
+    calculator.isWaitForSecondNumber = false;
+}
+
+function inputDigit(digit) {
+    calculator.displayNumber += digit;
+}
 
