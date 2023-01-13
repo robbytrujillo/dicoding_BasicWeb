63f11e562bba3c20cdb99145ff0e6352fdf3ce36
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-01-14 06:44:48 +0700

    Dicoding Web Basic - Dasar JS - Menerpakan JS pada Web kalkulator

--- belajar/WebCalculator/script/kalkulator.js
+++ belajar/WebCalculator/script/kalkulator.js
@@ -23,3 +23,15 @@ function inputDigit(digit) {
     calculator.displayNumber += digit;
 }
 
+const buttons = document.querySelectorAll('.button');
+
+for (const button of buttons) {
+    button.addEventListener('click', function (event) {
+        //mendapatkan objek elemen yang diklik
+        const target = event.target;
+
+        inputDigit(target.innerText);
+        updateDisplay();
+    });
+}
+
