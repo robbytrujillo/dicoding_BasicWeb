f8c387196a5a57dbb8793932719b227bd5eaed2d
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-01-13 14:09:52 +0700

    Dicoding Web Basic - Dasar JS - variabel

---
+++ belajar/WebCalculator/script/variabel.js
@@ -0,0 +1,19 @@
+// ini merupakan komentar satu baris komentar
+// alert("Terima kasih")
+
+/* Ini merupakan komentar dengan lebih dari satu baris
+Teks apapun yang berada disini akan dijadikan komentar.
+Ketika menggunakan ini, jangan lupa untuk menutupnya.
+*/
+
+//variable
+var firstName = "Harry";
+console.log(firstName);
+
+firstName = "Ron";
+console.log(firstName);
+
+/* output: 
+Harry
+Ron
+*/
