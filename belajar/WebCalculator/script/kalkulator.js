82cf9d5d3e01685de0d2ff970bf8434e384b784a
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-01-16 05:36:44 +0700

    Dicoding Web Basic - Dasar JS - Up Menerpakan JS pada Web kalkulator

--- belajar/WebCalculator/script/kalkulator.js
+++ belajar/WebCalculator/script/kalkulator.js
@@ -34,19 +34,26 @@ for (const button of buttons) {
         //mendapatkan objek elemen yang diklik
         const target = event.target;
 
-        inputDigit(target.innerText);
-        updateDisplay();
-    });
-}
-
-for (const button of buttons) {
-    button.addEventListener('click', function(event) {
-        //mendapatkab objek elemen yang diklik
-        const target = event.target;
-
         if (target.classList.contains('clear')) {
             clearCalculator();
             updateDisplay();
+            return;
+        }
+
+        if (target.classList.contains('negative')) {
+            inverseNumber();
+            updateDisplay();
+            return;
+        }
+
+        if (target.classList.contains('equals')) {
+            performCalculation();
+            updateDisplay();
+            return;
+        }
+
+        if(target.classList.contains('operator')) {
+            handleOperator(target.innerText);
             return;
         }
 
@@ -54,4 +61,41 @@ for (const button of buttons) {
         updateDisplay();
     });
 }
+
+// for (const button of buttons) {
+//     button.addEventListener('click', function(event) {
+//         //mendapatkab objek elemen yang diklik
+//         const target = event.target;
+
+//         if (target.classList.contains('clear')) {
+//             clearCalculator();
+//             updateDisplay();
+//             return;
+//         }
+
+//         inputDigit(target.innerText);
+//         updateDisplay();
+//     });
+// }
+
+function inverseNumber() {
+    if (calculator.displayNumber === '0') {
+        return;
+    }
+    calculator.displayNumber = calculator.displayNumber * -1;
+}
+
+function handleOperator(operator) {
+    if (!calculator.isWaitForSecondNumber) {
+        calculator.operator = operator;
+        calculator.isWaitForSecondNumber = true;
+        calculator.firstNumber = calculator.displayNumber;
+
+        // mengatur ulang nilai display number supaya tombol selanjutnya dimulai dari angka pertama lagi
+        calculator.displayNumber = '0';
+    } else {
+        alert('Operator sudah ditetapkan');
+    }
+    }
+
 
